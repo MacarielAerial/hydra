5db2ed0d0ce2bdd35e9c05ba5ef6712c79c75710
root <my_name@example.com>
2022-08-06 23:47:42 +0100

    Add unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,25 @@ from logging import Logger
 from pathlib import Path
 
 import en_core_web_sm
+from networkx import DiGraph
 from pytest import fixture
 from spacy.language import Language
 from spacy.tokens import Doc, Span
 
 from src.hydra.nodes.base_logger import get_base_logger
+from src.hydra.nodes.linguistic_graph_edges import (
+    BaseEdgeFeats,
+    EdgeTuple,
+    EdgeTuples,
+    EdgeType,
+)
+from src.hydra.nodes.linguistic_graph_nodes import (
+    BaseNodeFeats,
+    NodeTuple,
+    NodeTuples,
+    NodeType,
+    TokenNodeFeats,
+)
 
 
 @fixture
@@ -63,3 +77,86 @@ class TestFixture:
         sent = list(self.example_paragraph_doc.sents)[0]
 
         return sent
+
+    @property
+    def example_node_tuples(self) -> NodeTuples:
+        node_tuples = NodeTuples(
+            list_node_tuple=[
+                NodeTuple(
+                    node_id=0,
+                    node_feats=TokenNodeFeats(
+                        ntype=NodeType.token, text="New York", position_id=0
+                    ),
+                ),
+                NodeTuple(
+                    node_id=1,
+                    node_feats=TokenNodeFeats(
+                        ntype=NodeType.token, text="City", position_id=1
+                    ),
+                ),
+                NodeTuple(
+                    node_id=2,
+                    node_feats=TokenNodeFeats(
+                        ntype=NodeType.token, text="is", position_id=2
+                    ),
+                ),
+                NodeTuple(
+                    node_id=3,
+                    node_feats=TokenNodeFeats(
+                        ntype=NodeType.token, text="in", position_id=3
+                    ),
+                ),
+                NodeTuple(
+                    node_id=4,
+                    node_feats=TokenNodeFeats(
+                        ntype=NodeType.token, text="New York", position_id=4
+                    ),
+                ),
+                NodeTuple(
+                    node_id=5,
+                    node_feats=BaseNodeFeats(
+                        ntype=NodeType.sentence,
+                        text="New York City is in New York",
+                    ),
+                ),
+            ]
+        )
+
+        return node_tuples
+
+    @property
+    def example_edge_tuples(self) -> EdgeTuples:
+        edge_tuples = EdgeTuples(
+            list_edge_tuple=[
+                EdgeTuple(
+                    src_id=0,
+                    dst_id=5,
+                    edge_feats=BaseEdgeFeats(etype=EdgeType.token_to_sent, text=""),
+                ),
+                EdgeTuple(
+                    src_id=4,
+                    dst_id=5,
+                    edge_feats=BaseEdgeFeats(etype=EdgeType.token_to_sent, text=""),
+                ),
+            ]
+        )
+
+        return edge_tuples
+
+    @property
+    def example_nx_g_for_contraction_by_text(  # type: ignore[no-any-unimported]
+        self,
+    ) -> DiGraph:
+        nx_g = DiGraph()
+        nx_g.add_nodes_from(
+            [
+                (0, {"ntype": "TOKEN", "text": "New York"}),
+                (1, {"ntype": "TOKEN", "text": "New York"}),
+                (2, {"ntype": "SENTENCE", "text": "New York New York"}),
+            ]
+        )
+        nx_g.add_edges_from(
+            [(0, 2, {"etype": "TokenToSent"}), (1, 2, {"etype": "TokenToSent"})]
+        )
+
+        return nx_g
--- tests/unit/test_linguistic_graph_construction.py
+++ tests/unit/test_linguistic_graph_construction.py
@@ -1,8 +1,11 @@
 from logging import Logger
 
 from src.hydra.nodes.linguistic_graph_construction import (
+    build_graph_from_node_tuples_and_edge_tuples,
     collect_sent_graph_elements_from_spacy,
+    contract_ntype_nodes_by_identical_text,
 )
+from src.hydra.nodes.linguistic_graph_nodes import NodeType
 from tests.conftest import TestFixture
 
 
@@ -15,3 +18,29 @@ def test_collect_sent_graph_from_spacy(
 
     assert len(node_tuples.list_node_tuple) > 0
     assert len(edge_tuples.list_edge_tuple) > 0
+
+
+def test_build_graph_from_node_tuples_and_edge_tuples(
+    test_logger: Logger, test_fixture: TestFixture
+) -> None:
+    nx_g = build_graph_from_node_tuples_and_edge_tuples(
+        node_tuples=test_fixture.example_node_tuples,
+        edge_tuples=test_fixture.example_edge_tuples,
+        logger=test_logger,
+    )
+
+    assert len(nx_g.nodes) == 6
+    assert len(nx_g.edges) == 2
+
+
+def test_contract_ntype_nodes_by_identical_text(
+    test_logger: Logger, test_fixture: TestFixture
+) -> None:
+    nx_g = contract_ntype_nodes_by_identical_text(
+        nx_g=test_fixture.example_nx_g_for_contraction_by_text,
+        ntype=NodeType.token,
+        logger=test_logger,
+    )
+
+    assert len(nx_g.nodes) == 2
+    assert len(nx_g.edges) == 1
